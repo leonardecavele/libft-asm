09529d9f3a8d1559dc5e3df7575eabe6826e22b9
leonardecavele <leonard.decavele@gmail.com>
2025-10-27 18:58:23 +0100

    renamed 'helloworld.asm' to 'main.asm'

--- main.asm
+++ main.asm
@@ -2,6 +2,7 @@ global _start
 extern putstr
 extern strlen
 extern itoa
+extern putchar
 
 segment	.data
 hw db "Hello world", 10, 0
@@ -9,22 +10,28 @@ hw db "Hello world", 10, 0
 segment .text
 _start:
 	mov rdi, hw	
-	call putstr		; hw
+	call putstr
 
 	mov rdi, hw
 	call strlen
-
 	mov rdi, rax
 	call itoa
-
 	mov rdi, rax
-	call putstr		; len of 'hw'
+	call putstr
+	mov rdi, 10
+	call putchar
 
 	mov rdi, -2147483648 
 	call itoa
-
 	mov rdi, rax
-	call putstr		; upper number
+	call putstr
+	mov rdi, 10
+	call putchar
+
+	mov rdi, 60
+	call putchar
+	mov rdi, 10
+	call putchar
 
 	mov rax, 60
 	mov	rdi, 0
